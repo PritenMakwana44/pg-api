bd9433872c00a5204c9a97bfdfd92bc77ce56791
PritenMakwana44 <priten.makwana7@gmail.com>
2023-02-26 12:59:56 +0000

    done

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,9 +7,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
-    posts_count = serializersReadOnlyField()
-    followers_count = serializersReadOnlyField()
-    following_count = serializersReadOnlyField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
